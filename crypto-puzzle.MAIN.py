06d5fedd29d1b1467517f9193cb4f89419d3f7a7
unknown <henrik.peters.98@web.de>
2018-01-14 17:04:49 +0100

    implemented input handling

--- crypto-puzzle.MAIN.py
+++ crypto-puzzle.MAIN.py
@@ -1,1 +1,58 @@
 # Exercise 11.3: Cryptoarithmetical Puzzle
+# opening a text file with exactly one crypto-puzzle in it
+with open("crypto-puzzle-data.txt", encoding="utf-8") as f:
+    content = f.readlines()
+
+
+# function to format the file
+# formatting the file into a usable form.
+# first of all a the complete string is divided into substring consisting of exactly one line in the file.
+# second these substrings are transformed into a list of chars.
+# third all useless characters like ' ' are removed
+def format_input(con):
+    con = [x.strip() for x in con]
+    con = [list(x) for x in con]
+    con = [[elem for elem in x if elem != ' '] for x in con]
+    return con
+
+
+# formatting the content
+content = format_input(content)
+
+
+# function to define the order of operations.
+# function iterates over the list of character(content) and checks if one of these characters is an operation
+# if this is true, this operations sign is added to the op_order list.
+# currently not implemented: prioritising point over line operations
+def create_operation_order(con):
+    op_order = []
+    for x in con:
+        for y in x:
+            if y == '+' or y == '-' or y == '*' or y == '/':
+                op_order.append(y)
+    return op_order
+
+
+# creating the operations order
+global_oo = create_operation_order(content)
+
+
+# creates a dictionary of the variables in the puzzle so that every variable is a key in the dictionary
+# all operation symbols as well as the equality lines are removed from the dict
+def create_vars(con):
+    var_dict = {}
+    for x in con:
+        for y in x:
+            if y not in var_dict.keys():
+                var_dict[y] = 0
+    var_dict.pop('+', None)
+    var_dict.pop('-', None)
+    var_dict.pop('*', None)
+    var_dict.pop('/', None)
+    var_dict.pop('_', None)
+    return var_dict
+
+
+global_vd = create_vars(content)
+print(global_vd)
+print(content)
