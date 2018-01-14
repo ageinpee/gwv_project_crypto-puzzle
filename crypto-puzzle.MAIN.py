4f31e95a0cd18675f459ceee1bb2897ada68d4a5
unknown <henrik.peters.98@web.de>
2018-01-14 17:57:53 +0100

    started implementation of vertical vars

--- crypto-puzzle.MAIN.py
+++ crypto-puzzle.MAIN.py
@@ -37,22 +37,54 @@ def create_operation_order(con):
 global_oo = create_operation_order(content)
 
 
+# filtering all operation symbols from the content
+# '+', '-', '*' and '/' are removed
+def filter_operations(con):
+    con = [[elem for elem in x if elem != '+'] for x in con]
+    con = [[elem for elem in x if elem != '-'] for x in con]
+    con = [[elem for elem in x if elem != '*'] for x in con]
+    con = [[elem for elem in x if elem != '/'] for x in con]
+    return con
+
+
+# filtering operation symbols
+content = filter_operations(content)
+
+
 # creates a dictionary of the variables in the puzzle so that every variable is a key in the dictionary
-# all operation symbols as well as the equality lines are removed from the dict
+# underscore lines ('_') are removed from the dict
 def create_vars(con):
     var_dict = {}
     for x in con:
         for y in x:
             if y not in var_dict.keys():
                 var_dict[y] = 0
-    var_dict.pop('+', None)
-    var_dict.pop('-', None)
-    var_dict.pop('*', None)
-    var_dict.pop('/', None)
     var_dict.pop('_', None)
     return var_dict
 
 
+# creating the variable dictionary
 global_vd = create_vars(content)
+
+
+# creating a list of lists with all the vertical alignments in the equation
+#def create_vertical_vars(con):
+#    vertical_vars = []
+#    flag = False
+#    for x in con:
+#        if len(x) < len(max(con,key=len)):
+#            for i in range(0, len(max(con,key=len))-len(x)):
+#                if flag == False:
+#                    vertical_vars.append([x[' ']])
+#                if flag == True:
+#                    vertical_vars[].append()
+#        for i in range(len(x)-1, 0):
+#            if flag == False:
+#                vertical_vars.append([x[i]])
+
+
+
+# printing global variables
+print(global_oo)
 print(global_vd)
 print(content)
